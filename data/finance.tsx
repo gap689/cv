d75b140383fec9a3c1afbc29d8796de487bf5b97
German Alamilla Peralta <german.alamilla@gmail.com>
2024-01-25 23:01:18 -0600

    🌱  (data) added seed data

---
+++ data/finance.tsx
@@ -0,0 +1,618 @@
+export const assetBalances = [
+    {
+        "asset": "XAI",
+        "assetName": "Xai",
+        "free": "0.05157897",
+        "locked": "0",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20240103/a18e02bc-282b-4a44-ab6c-6e20b89a4132.png",
+        "transferBtc": "0.00000094"
+    },
+    {
+        "asset": "SHIB",
+        "assetName": "SHIBA INU",
+        "free": "3227699.65",
+        "locked": "0",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20210510/028e07ac-4117-4ddf-b9fd-90d8bd1c9406.png",
+        "transferBtc": "0.00071655"
+    },
+    {
+        "asset": "ALT",
+        "assetName": "AltLayer",
+        "free": "21.16652757",
+        "locked": "0",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20240117/5b268e58-53f4-48cb-b78f-13b63be5543d.png",
+        "transferBtc": "0"
+    },
+    {
+        "asset": "MEME",
+        "assetName": "Memecoin",
+        "free": "72.93268147",
+        "locked": "0",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20231027/1fe7a9d3-2f97-4a81-b6b0-51e9c7d1e6af.png",
+        "transferBtc": "0.00004151"
+    },
+    {
+        "asset": "XRP",
+        "assetName": "Ripple",
+        "free": "0.00000039",
+        "locked": "0",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20201110/4766a9cc-8545-4c2b-bfa4-cad2be91c135.png",
+        "transferBtc": "0"
+    },
+    {
+        "asset": "AI",
+        "assetName": "Sleepless AI",
+        "free": "0.22336563",
+        "locked": "0",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20231226/9b31c2c5-08df-4352-bd0f-34c2fcbad763.png",
+        "transferBtc": "0.00000571"
+    },
+    {
+        "asset": "BTC",
+        "assetName": "Bitcoin",
+        "free": "0.00953369",
+        "locked": "0",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20201110/87496d50-2408-43e1-ad4c-78b47b448a6a.png",
+        "transferBtc": "0.00953369"
+    },
+    {
+        "asset": "ETHW",
+        "assetName": "Ethereum PoW",
+        "free": "0.00930127",
+        "locked": "0",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20220915/8a5f5fe0-efed-4b22-ab1c-42f9ed12d812.png",
+        "transferBtc": "0.00000061"
+    },
+    {
+        "asset": "LUNA",
+        "assetName": "Terra",
+        "free": "0.00001112",
+        "locked": "0",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20230310/a4966b95-103e-4ef5-8641-c85fdc2601c7.png",
+        "transferBtc": "0"
+    },
+    {
+        "asset": "BNB",
+        "assetName": "BNB",
+        "free": "0.00007946",
+        "locked": "0",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20220218/94863af2-c980-42cf-a139-7b9f462a36c2.png",
+        "transferBtc": "0.00000059"
+    },
+    {
+        "asset": "MANTA",
+        "assetName": "Manta",
+        "free": "0.0654184",
+        "locked": "1.7",
+        "freeze": "0",
+        "withdrawing": "0",
+        "logoUrl": "https://bin.bnbstatic.com/image/admin_mgs_image_upload/20240115/10e66475-7a42-4a81-8943-5afb1f61f538.png",
+        "transferBtc": "0.00011288"
+    }
+]
+
+export const userProfits = [
+    {
+        "userId": 40252212,
+        "day": 1703462400000,
+        "balance": 159.76779837,
+        "netInflow": 0.00000000,
+        "profit": 0.87873500,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1703548800000,
+        "balance": 161.68347242,
+        "netInflow": 0.00000000,
+        "profit": 1.91567405,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1703635200000,
+        "balance": 175.20500047,
+        "netInflow": 13.00088505,
+        "profit": 0.52064300,
+        "debt": 0.00000000,
+        "transferIn": 13.00088505
+    },
+    {
+        "userId": 40252212,
+        "day": 1703721600000,
+        "balance": 121.32770696,
+        "netInflow": -56.00000000,
+        "profit": 2.12270649,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1703808000000,
+        "balance": 119.47839243,
+        "netInflow": 0.00000000,
+        "profit": -1.84931453,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1703894400000,
+        "balance": 119.47839243,
+        "netInflow": 0.00000000,
+        "profit": 0.00000000,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1703980800000,
+        "balance": 41.12347310,
+        "netInflow": 61.56384832,
+        "profit": -139.91876765,
+        "debt": 0.00000000,
+        "transferIn": 71.56384832
+    },
+    {
+        "userId": 40252212,
+        "day": 1704067200000,
+        "balance": 41.12347310,
+        "netInflow": 0.00000000,
+        "profit": 0.00000000,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1704153600000,
+        "balance": 42.52372257,
+        "netInflow": 0.00000000,
+        "profit": 1.40024947,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1704240000000,
+        "balance": 42.52372257,
+        "netInflow": 0.00000000,
+        "profit": 0.00000000,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1704326400000,
+        "balance": 43.39604174,
+        "netInflow": 0.00000411,
+        "profit": 0.87231506,
+        "debt": 0.00000000,
+        "transferIn": 28.00000411
+    },
+    {
+        "userId": 40252212,
+        "day": 1704412800000,
+        "balance": 43.39604174,
+        "netInflow": 0.00000000,
+        "profit": 0.00000000,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1704499200000,
+        "balance": 43.36453859,
+        "netInflow": 0.00000000,
+        "profit": -0.03150315,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1704585600000,
+        "balance": 43.94296329,
+        "netInflow": 0.00000000,
+        "profit": 0.57842470,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1704672000000,
+        "balance": 47.03018245,
+        "netInflow": 0.00000000,
+        "profit": 3.08721916,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1704758400000,
+        "balance": 49.76050845,
+        "netInflow": 0.00000000,
+        "profit": 2.73032600,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1704844800000,
+        "balance": 49.76050845,
+        "netInflow": 0.00000000,
+        "profit": 0.00000000,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1704931200000,
+        "balance": 48.50056567,
+        "netInflow": 0.00000000,
+        "profit": -1.25994278,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705017600000,
+        "balance": 48.50056567,
+        "netInflow": 8.14901671,
+        "profit": -8.14901671,
+        "debt": 0.00000000,
+        "transferIn": 843.77948859
+    },
+    {
+        "userId": 40252212,
+        "day": 1705104000000,
+        "balance": 48.50056567,
+        "netInflow": 0.00000000,
+        "profit": 0.00000000,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705190400000,
+        "balance": 49.04410675,
+        "netInflow": 0.00000000,
+        "profit": 0.54354108,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705276800000,
+        "balance": 49.26346993,
+        "netInflow": 0.00000000,
+        "profit": 0.21936318,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705363200000,
+        "balance": 49.56784645,
+        "netInflow": 0.00000000,
+        "profit": 0.30437652,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705449600000,
+        "balance": 50.02087787,
+        "netInflow": 0.00000000,
+        "profit": 0.45303142,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705536000000,
+        "balance": 50.26162567,
+        "netInflow": 0.00000000,
+        "profit": 0.24074780,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705622400000,
+        "balance": 50.60569433,
+        "netInflow": 0.00000000,
+        "profit": 0.34406866,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705708800000,
+        "balance": 50.79556086,
+        "netInflow": 0.00000000,
+        "profit": 0.18986653,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705795200000,
+        "balance": 51.37657932,
+        "netInflow": 0.00000000,
+        "profit": 0.58101846,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705881600000,
+        "balance": 51.61156111,
+        "netInflow": 0.00000000,
+        "profit": 0.23498179,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1705968000000,
+        "balance": 51.78706698,
+        "netInflow": 0.00000000,
+        "profit": 0.17550587,
+        "debt": 0.00000000,
+        "transferIn": 0.00000000
+    },
+    {
+        "userId": 40252212,
+        "day": 1706054400000,
+        "balance": 51.83978548,
+        "netInflow": 0.00000000,
+        "profit": 0.05271850,
+        "debt": null,
+        "transferIn": 0.00000000
+    }
+]
+
+export const pnlData = {
+"data": {
+    "userProfitRets": [
+        {
+            "userId": 40252212,
+            "day": 1704067200000,
+            "balance": 41.12347310,
+            "netInflow": 0.00000000,
+            "profit": 0.00000000,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1704153600000,
+            "balance": 42.52372257,
+            "netInflow": 0.00000000,
+            "profit": 1.40024947,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1704240000000,
+            "balance": 42.52372257,
+            "netInflow": 0.00000000,
+            "profit": 0.00000000,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1704326400000,
+            "balance": 43.39604174,
+            "netInflow": 0.00000411,
+            "profit": 0.87231506,
+            "debt": 0.00000000,
+            "transferIn": 28.00000411
+        },
+        {
+            "userId": 40252212,
+            "day": 1704412800000,
+            "balance": 43.39604174,
+            "netInflow": 0.00000000,
+            "profit": 0.00000000,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1704499200000,
+            "balance": 43.36453859,
+            "netInflow": 0.00000000,
+            "profit": -0.03150315,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1704585600000,
+            "balance": 43.94296329,
+            "netInflow": 0.00000000,
+            "profit": 0.57842470,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1704672000000,
+            "balance": 47.03018245,
+            "netInflow": 0.00000000,
+            "profit": 3.08721916,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1704758400000,
+            "balance": 49.76050845,
+            "netInflow": 0.00000000,
+            "profit": 2.73032600,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1704844800000,
+            "balance": 49.76050845,
+            "netInflow": 0.00000000,
+            "profit": 0.00000000,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1704931200000,
+            "balance": 48.50056567,
+            "netInflow": 0.00000000,
+            "profit": -1.25994278,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705017600000,
+            "balance": 48.50056567,
+            "netInflow": 8.14901671,
+            "profit": -8.14901671,
+            "debt": 0.00000000,
+            "transferIn": 843.77948859
+        },
+        {
+            "userId": 40252212,
+            "day": 1705104000000,
+            "balance": 48.50056567,
+            "netInflow": 0.00000000,
+            "profit": 0.00000000,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705190400000,
+            "balance": 49.04410675,
+            "netInflow": 0.00000000,
+            "profit": 0.54354108,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705276800000,
+            "balance": 49.26346993,
+            "netInflow": 0.00000000,
+            "profit": 0.21936318,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705363200000,
+            "balance": 49.56784645,
+            "netInflow": 0.00000000,
+            "profit": 0.30437652,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705449600000,
+            "balance": 50.02087787,
+            "netInflow": 0.00000000,
+            "profit": 0.45303142,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705536000000,
+            "balance": 50.26162567,
+            "netInflow": 0.00000000,
+            "profit": 0.24074780,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705622400000,
+            "balance": 50.60569433,
+            "netInflow": 0.00000000,
+            "profit": 0.34406866,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705708800000,
+            "balance": 50.79556086,
+            "netInflow": 0.00000000,
+            "profit": 0.18986653,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705795200000,
+            "balance": 51.37657932,
+            "netInflow": 0.00000000,
+            "profit": 0.58101846,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705881600000,
+            "balance": 51.61156111,
+            "netInflow": 0.00000000,
+            "profit": 0.23498179,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        },
+        {
+            "userId": 40252212,
+            "day": 1705968000000,
+            "balance": 51.78706698,
+            "netInflow": 0.00000000,
+            "profit": 0.17550587,
+            "debt": 0.00000000,
+            "transferIn": 0.00000000
+        }
+    ],
+    "totalProfit": 11.95503570,
+    "totalLoss": 149.35923029,
+    "netProfit": -137.40419459,
+    "profitDays": 15,
+    "lossDays": 4,
+    "fairDays": 5,
+    "winDaysRate": 62.5,
+    "averageProfit": 0.79700238,
+    "averageLoss": 37.33980757,
+    "profitRate": 0.02134458
+    }
+}
