d8f481d51d8071774cab60e4e7c8a04a50e1f1ba
German Alamilla Peralta <german.alamilla@gmail.com>
2024-02-10 11:37:45 -0600

    🚧  ui(page) experimental page

---
+++ app/app/experiments/page.tsx
@@ -0,0 +1,13 @@
+import { FlaskConical } from "lucide-react";
+
+const ExperimentsPage = () => {
+    return ( 
+        <div className="w-full h-full flex flex-col items-center justify-center">
+            <div className="flex text-2xl uppercase items-center">
+                <FlaskConical className="w-6 h-6 mr-3"/> In progress
+            </div>
+        </div>
+     );
+}
+ 
+export default ExperimentsPage;
