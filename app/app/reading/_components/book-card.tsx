e453f1a8c00a74bc4b010f9069e808a4bb1a2950
German Alamilla Peralta <german.alamilla@gmail.com>
2024-02-10 19:48:34 -0600

    🚧  ui(page) reading list page

---
+++ app/app/reading/_components/book-card.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image"
+
+import { cn } from "@/lib/utils"
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu"
+
+import { Library } from "@/data/reading/books"
+import { PlusCircle } from "lucide-react"
+
+interface BookCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  book: Library
+  aspectRatio?: "portrait" | "square"
+  width?: number
+  height?: number
+}
+
+export function BookCard({
+  book,
+  aspectRatio = "portrait",
+  width,
+  height,
+  className,
+  ...props
+}: BookCardProps) {
+  return (
+    <div className={cn("space-y-3", className)} {...props}>
+      <ContextMenu>
+        <ContextMenuTrigger>
+          <div className="overflow-hidden rounded-md">
+            <Image
+              src={book.cover}
+              alt={book.name}
+              width={width}
+              height={height}
+              className={cn(
+                "h-auto w-auto object-cover transition-all hover:scale-105",
+                aspectRatio === "portrait" ? "aspect-[3/4]" : "aspect-square"
+              )}
+            />
+          </div>
+        </ContextMenuTrigger>
+        <ContextMenuContent className="w-40">
+          <ContextMenuItem>Add to Library</ContextMenuItem>
+          <ContextMenuSub>
+            <ContextMenuSubTrigger>Add to Playlist</ContextMenuSubTrigger>
+            <ContextMenuSubContent className="w-48">
+              <ContextMenuItem>
+                <PlusCircle className="mr-2 h-4 w-4" />
+                New List
+              </ContextMenuItem>
+              <ContextMenuSeparator />
+            </ContextMenuSubContent>
+          </ContextMenuSub>
+          <ContextMenuSeparator />
+          <ContextMenuItem>Like</ContextMenuItem>
+          <ContextMenuItem>Share</ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+      <div className="space-y-1 text-sm">
+        <h3 className="font-medium leading-none">{book.name}</h3>
+        <p className="text-xs text-muted-foreground">{book.author}</p>
+      </div>
+    </div>
+  )
+}
---
+++ app/app/reading/page.tsx
@@ -0,0 +1,135 @@
+import { Metadata } from "next"
+
+import { Button } from "@/components/ui/button"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/separator"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
+import { readingNow, nextReading, readingNowElse } from "@/data/reading/books"
+import { PlusCircle } from "lucide-react"
+import { BookCard } from "./_components/book-card"
+
+export const metadata: Metadata = {
+  title: "Music App",
+  description: "Example music app using the components.",
+}
+
+export default function ReadingPage() {
+  return (
+    <>
+      <div className="block">
+        <div className="grid lg:grid-cols-4">
+          
+          <div className="col-span-3 lg:col-span-4">
+            <div className="h-full px-4 py-6 lg:px-8">
+              <div className="flex items-center justify-between pb-4">
+                <div className="space-y-1">
+                  <h2 className="text-2xl font-semibold tracking-tight">
+                    Reading List
+                  </h2>
+                  <p className="text-sm text-muted-foreground">
+                    Top picks for you. Updated monthly.
+                  </p>
+                </div>
+              </div>
+              <Tabs defaultValue="programming" className="space-y-6">
+                <div className="space-between flex items-center">
+                  <TabsList>
+                    <TabsTrigger value="programming" className="relative">
+                      Programming
+                    </TabsTrigger>
+                    <TabsTrigger value="else">
+                      Anything else
+                    </TabsTrigger>
+                  </TabsList>
+                </div>
+
+                <Separator className="my-4"/>
+
+                <TabsContent
+                  value="programming"
+                  className="border-none p-0 outline-none"
+                >
+                  <div className="relative">
+                    <ScrollArea>
+                      <div className="flex space-x-4 pb-4">
+                        {readingNow.map((book) => (
+                          <BookCard
+                            key={book.name}
+                            book={book}
+                            className="w-[250px]"
+                            aspectRatio="portrait"
+                            width={250}
+                            height={330}
+                          />
+                        ))}
+                      </div>
+                      <ScrollBar orientation="horizontal" />
+                    </ScrollArea>
+                  </div>
+                </TabsContent>
+
+                <TabsContent
+                  value="else"
+                  className="h-full flex-col border-none p-0 data-[state=active]:flex"
+                >
+                  <div className="flex items-center justify-between">
+                    <div className="relative">
+                      <ScrollArea>
+                        <div className="flex space-x-4 pb-4">
+                          {readingNowElse.map((book) => (
+                            <BookCard
+                              key={book.name}
+                              book={book}
+                              className="w-[250px]"
+                              aspectRatio="portrait"
+                              width={250}
+                              height={330}
+                            />
+                          ))}
+                        </div>
+                        <ScrollBar orientation="horizontal" />
+                      </ScrollArea>
+                    </div>
+                  </div>                  
+                </TabsContent>
+              </Tabs>
+
+              <div className="mt-6 space-y-1">
+                    <h2 className="text-2xl font-semibold tracking-tight">
+                      Want to read
+                    </h2>
+                    <p className="text-sm text-muted-foreground">
+                      A list of books that I want to read
+                    </p>
+                  </div>
+                  <Separator className="my-4" />
+                  <div className="relative">
+                    <ScrollArea>
+                      <div className="flex space-x-4 pb-4">
+                        {nextReading.map((book) => (
+                          <BookCard
+                            key={book.name}
+                            book={book}
+                            className="w-[150px]"
+                            aspectRatio="square"
+                            width={150}
+                            height={150}
+                          />
+                        ))}
+                      </div>
+                      <ScrollBar orientation="horizontal" />
+                    </ScrollArea>
+                  </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
