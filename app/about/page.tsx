63251d9fae164acc37a92040d2a376cd46356330
German Alamilla Peralta <german.alamilla@gmail.com>
2024-03-01 17:52:31 -0600

    🚀  about page, my technology manifesto

---
+++ app/about/page.tsx
@@ -0,0 +1,85 @@
+const AboutPage = () => {
+    return ( 
+        <div className="pt-14 min-h-full md:p-8 p-4 flex flex-col items-center">
+            <div className="2xl:max-w-7xl xl:max-w-5xl pt-10">
+                <div className="md:text-6xl text-3xl font-semibold uppercase py-10">
+                    A Manifesto for a Technological Renaissance
+                </div>
+                
+                <div className="grid lg:grid-cols-3 grid-cols-2 grid-rows-2 lg:py-8">
+                    <div className="col-span-1 flex justify-center items-center h-auto row-span-2">
+                            <p className="[writing-mode:tb] transform -rotate-180 text-3xl inline-block uppercase">
+                                Manifesto
+                            </p>
+                    </div>
+
+                    <div className="col-span-1 [writing-mode:tb] transform -rotate-180 text-2xl p-4 row-span-1 flex justify-center">Progress</div>
+
+                    <div className="col-span-1 md:text-lg p-4 row-span-1 text-muted-foreground">In the grand tapestry of human progress, technology stands as our most potent brush, capable of painting both the brightest utopias and the darkest dystopias. As inheritors of this legacy, we must heed the wisdom of philosophers, writers, scientists, and intellectuals who have illuminated the path forward.</div>
+
+                    <div className="col-span-1 md:text-lg p-4 row-span-1 text-muted-foreground">As we advance, let us remember that technology is not inherently good or evil; it is the application that determines its moral worth. We must embed ethical considerations into the very fabric of our innovations, ensuring that every advancement serves humanity's collective well-being.</div>
+
+                    <div className="col-span-1 [writing-mode:tb] transform -rotate-180 text-2xl p-4 row-span-1 flex justify-center">Technology</div>
+
+                </div>
+                <div className="grid md:grid-cols-2 grid-cols-1 py-10 px-4">
+                    <div className="col-span-1 p-3">
+                        <h2 className="font-semibold">The Pursuit of Knowledge</h2>
+                        <div className="text-sm p-4 text-muted-foreground">
+                            Like Carl Sagan, let us embrace the spirit of curiosity and exploration. Our quest for knowledge should know no bounds, extending from the infinitesimal realms of quantum mechanics to the vast expanse of the cosmos. Let us cultivate a society that reveres education, critical thinking, and scientific literacy as the cornerstones of progress.
+                        </div>
+                    </div>
+                    <div className="col-span-1 p-3">
+                        <h2 className="font-semibold">The Link of Science and Spirituality</h2>
+                        <div className="text-sm p-4 text-muted-foreground">
+                            Recognizing the interconnectedness of all things, we must bridge the perceived chasm between science and spirituality. Like Neil deGrasse Tyson, let us seek wonder and awe in the mysteries of the universe, finding solace in the beauty of cosmic evolution and the delicate balance of life on Earth.
+                        </div>
+                    </div>
+                    <div className="col-span-1 p-3">
+                        <h2 className="font-semibold">The Power of Imagination</h2>
+                        <div className="text-sm p-4 text-muted-foreground">
+                            In the words of Isaac Asimov, "The most exciting phrase to hear in science, the one that heralds new discoveries, is not 'Eureka!' but 'That's funny...'" Let us nurture the boundless creativity of the human mind, embracing imagination as the spark that ignites innovation and fuels progress.
+                        </div>
+                    </div>
+                    <div className="col-span-1 p-3">
+                        <h2 className="font-semibold">
+                            The Responsibility of Stewardship
+                        </h2>
+                        <div className="text-sm p-4 text-muted-foreground">
+                            Like Michio Kaku, let us envision a Type I civilization, capable of harnessing the full potential of our planet's resources in harmony with nature. We are custodians of Earth, entrusted with its care for future generations. Our technological prowess must be wielded with wisdom and foresight, preserving the delicate balance of ecosystems and safeguarding the diversity of life.
+                        </div>
+                    </div>
+                    <div className="col-span-1 p-3">
+                        <h2 className="font-semibold">
+                            The Promise of Decentralization
+                        </h2>
+                        <div className="text-sm p-4 text-muted-foreground">
+                            Like Michio Kaku, let us envision a Type I civilization, capable of harnessing the full potential of our planet's resources in harmony with nature. We are custodians of Earth, entrusted with its care for future generations. Our technological prowess must be wielded with wisdom and foresight, preserving the delicate balance of ecosystems and safeguarding the diversity of life.
+                        </div>
+                    </div>
+                    <div className="col-span-1 p-3">
+                        <h2 className="font-semibold">
+                            The Imperative of Inclusivity
+                        </h2>
+                        <div className="text-sm p-4 text-muted-foreground">
+                            Like Michio Kaku, let us envision a Type I civilization, capable of harnessing the full potential of our planet's resources in harmony with nature. We are custodians of Earth, entrusted with its care for future generations. Our technological prowess must be wielded with wisdom and foresight, preserving the delicate balance of ecosystems and safeguarding the diversity of life.
+                        </div>
+                    </div>
+                    <div className="col-span-1 p-3">
+                        <h2 className="font-semibold">
+                            The Legacy of Enlightenment
+                        </h2>
+                        <div className="text-sm p-4 text-muted-foreground">
+                            As heirs to the Enlightenment, we inherit the mantle of progress and reason. Let us reject dogma and superstition, embracing evidence-based inquiry and rational discourse as the pillars of our intellectual endeavor.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="text-muted-foreground text-lg py-5 2xl:max-w-7xl xl:max-w-5xl">
+                In the crucible of technological evolution, we stand at a crossroads of unprecedented potential and peril. Let us heed the wisdom of those who came before us, forging a path guided by ethics, knowledge, unity, imagination, stewardship, decentralization, inclusivity, and enlightenment. Together, let us embark on a journey towards a brighter future for all humanity.
+            </div>
+        </div>
+     );
+}
+ 
+export default AboutPage;
