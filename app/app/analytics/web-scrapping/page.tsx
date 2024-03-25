0522442d744c68d92ce6b8d08d8074cb193be1bd
German Alamilla Peralta <german.alamilla@gmail.com>
2024-03-24 21:46:02 -0600

    🚧 ui(page) for web scrapping notebook

---
+++ app/app/analytics/web-scrapping/page.tsx
@@ -0,0 +1,12 @@
+import { FlaskConical } from "lucide-react";
+
+const WebScrappingPage = () => {
+    return ( 
+        <div className="h-full p-4 flex flex-col items-center justify-center">
+            <FlaskConical className="animate-pulse w-10 h-10"/>
+            Web Scrapping notebook in progress
+        </div>
+     );
+}
+ 
+export default WebScrappingPage;
