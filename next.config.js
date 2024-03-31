8c6e0a1ed1229cc3344b9c3cf64eb817587ab703
German Alamilla Peralta <german.alamilla@gmail.com>
2024-03-31 15:35:49 -0600

    configs, dependencies, installs, styles

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["m.media-amazon.com", "images.unsplash.com", "avatars.githubusercontent.com"]
-      }
+  images: {
+      domains: ["m.media-amazon.com", "images.unsplash.com", "avatars.githubusercontent.com"]
+    }
 }
 
 module.exports = nextConfig
