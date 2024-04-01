95a2c6a89020081c802eeb50c15a7b889a34aea0
German Alamilla Peralta <german.alamilla@gmail.com>
2024-03-31 18:01:29 -0600

    ui(page) initial analytics page

--- app/app/analytics/page.tsx
+++ app/app/analytics/page.tsx
@@ -3,16 +3,23 @@ import { Suspense } from "react";
 import { AnalyticsListLayout } from "./_components/analytics-list-layout";
 
 import { posts } from "@/data/analytics/data";
-import { ScrollArea } from "@/components/ui/scroll-area";
-
 
 export default async function Analytics() {
   
     return (
-      <ScrollArea className="flex flex-col md:hidden">
-        <Suspense fallback={<LoadingSpinner />}>
-          <AnalyticsListLayout list={posts} isMobile />
-        </Suspense>
-      </ScrollArea>
+      <div className="flex flex-col h-full">
+        <div className="hidden md:flex items-center justify-center h-full bg-grid-black/[0.2] dark:bg-grid-white/[0.2] relative">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
+          <h2 className="text-xl">
+            Select a notebook
+          </h2>
+        </div>
+        <div className="md:hidden">
+          <Suspense fallback={<LoadingSpinner />}>
+            <AnalyticsListLayout list={posts} isMobile />
+          </Suspense>
+        </div>
+      </div>
     )
   }
