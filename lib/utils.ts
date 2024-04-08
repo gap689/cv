5651d20c0d500453c765c293f4a74ca1afc8720e
German Alamilla Peralta <german.alamilla@gmail.com>
2024-04-07 23:29:18 -0600

    utils for getting time format

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getDateTimeFormat = (date: Date) => {
+  const dateObj = new Date(date)
+  return Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit'
+  }).format(dateObj)
+}
