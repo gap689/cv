6199dfd7db3747c0879fddcfc03e7b33dbc680b9
German Alamilla Peralta <german.alamilla@gmail.com>
2024-04-10 13:28:43 -0600

    📈 added Goolge analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import dynamic from 'next/dynamic'
+import Script from 'next/script';
 
 import { GeistSans } from 'geist/font/sans';
 import { ThemeProvider } from '@/components/providers/theme-provider'
@@ -24,6 +25,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+        <Script id='google-analytics'>
+          {
+            `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+  
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `
+          }
+        </Script>
+      </head>
       <body className={GeistSans.className}>
         <ThemeProvider
           attribute="class"
