b1f79b891ca103c5f46d05926be89a4b1e673f5d
German Alamilla Peralta <german.alamilla@gmail.com>
2024-05-19 00:24:35 -0600

    text size changes for small screens

--- app/app/table/_components/healthcare/data-table-pagination.tsx
+++ app/app/table/_components/healthcare/data-table-pagination.tsx
@@ -21,13 +21,13 @@ import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from "lucide-r
   }: DataTablePaginationProps<TData>) {
     return (
       <div className="flex items-center justify-between px-2">
-        <div className="flex-1 text-sm text-muted-foreground">
+        <div className="flex-1 sm:text-sm text-xs text-muted-foreground">
           {table.getFilteredSelectedRowModel().rows.length} of{" "}
           {table.getFilteredRowModel().rows.length} row(s) selected.
         </div>
-        <div className="flex items-center space-x-6 lg:space-x-8">
+        <div className="flex items-center space-x-2 sm:space-x-3 md:space-x-6 lg:space-x-8">
           <div className="flex items-center space-x-2">
-            <p className="text-sm font-medium">Rows per page</p>
+            <p className="sm:text-sm text-xs font-medium">Rows per page</p>
             <Select
               value={`${table.getState().pagination.pageSize}`}
               onValueChange={(value) => {
@@ -46,7 +46,7 @@ import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from "lucide-r
               </SelectContent>
             </Select>
           </div>
-          <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+          <div className="flex w-[100px] items-center justify-center sm:text-sm text-xs font-medium">
             Page {table.getState().pagination.pageIndex + 1} of{" "}
             {table.getPageCount()}
           </div>
