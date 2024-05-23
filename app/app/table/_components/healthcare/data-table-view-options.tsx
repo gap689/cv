1cb5b51726584daf00f99494d59b4377e87acc9f
German Alamilla Peralta <german.alamilla@gmail.com>
2024-05-23 01:09:21 -0600

    column visibility toggle

--- app/app/table/_components/healthcare/data-table-view-options.tsx
+++ app/app/table/_components/healthcare/data-table-view-options.tsx
@@ -21,13 +21,14 @@ interface DataTableViewOptionsProps<TData> {
 export function DataTableViewOptions<TData>({
   table,
 }: DataTableViewOptionsProps<TData>) {
+  
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
           variant="outline"
           size="sm"
-          className="ml-auto hidden h-8 lg:flex"
+          className="hidden h-8 md:flex"
         >
           <SlidersHorizontal className="mr-2 h-4 w-4" />
           View
